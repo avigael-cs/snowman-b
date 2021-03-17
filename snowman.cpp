cfd26dd43842272d1fd09646ccf511b34ca6ffab
avigael-cs <70202497+avigael-cs@users.noreply.github.com>
2021-03-17 10:26:45 +0200

    Add files via upload

--- snowman.cpp
+++ snowman.cpp
@@ -1,10 +1,21 @@
-
 #include <stdexcept>
 #include <vector>
 #include <iostream>
 using namespace std;
 
 namespace ariel{
+
+//the num
+const int zero = 0;    //hat
+const int one = 1;    //nose
+const int two = 2;    //left eye
+const int three = 3;  //right eye
+const int four = 4;   //left arm
+const int five = 5;   //right arm
+const int six = 6;    //torso
+const int seven = 7;  //base
+const int eight = 8;  //stam
+
 
 const vector<string> hat = {"_===_",
                             " ___ \n.....",
@@ -16,7 +27,7 @@ const vector<string> noses = {",",
                               "_",
                               " "};
 
-const vector<string> eyes = {".",
+const vector<string> EyesLandR = {".",
                              "o",
                              "O",
                              "-"};
@@ -35,94 +46,105 @@ const vector<string> torsos = {":",
                                "] [",
                                "> <",
                                "   "};
+
 const vector<string> body = {":",
                              "\" \"",
                              "___",""};
 
-string makeHead(char head){
-        int index = (head-'0')-1;
-        return hat[index]+'\n' ; 
+//level 1 of the snowman
+string printHead(char head){
+        return hat[(head-'0')-1]+'\n' ;
 }
 
-string makeNose(char nose){
-        int noseIndex = (nose -'0')-1;
-        string Nose = noses[noseIndex];
-        return Nose;
+string printNose(char nose){
+        return noses[(nose -'0')-1];
 }
 
-string makeFace(char nose, char l_Eye,char r_Eye){
-        int leftEye_i = (l_Eye-'0')-1;
-        int rightEye_i = (r_Eye-'0')-1;
-        string left = eyes[leftEye_i];
-        string right = eyes[rightEye_i];
-        return "("+left+makeNose(nose)+right+")+'\n'";
+//level 2 of the snowman
+string printFace(char nose, char l_Eye,char r_Eye){
+        string left = EyesLandR[(l_Eye-'0')-1];
+        string right = EyesLandR[(r_Eye-'0')-1];
+        return "("+left+printNose(nose)+right+")"+'\n';
 }
 
-string insertTorso(char t){
-        int torso_i = (t-'0')-1;
-        string tor = torsos[torso_i];
+//level 3 of the snowman
+string makeTorso(char t){
+        string torso = torsos[(t-'0')-1];
         string space = " ";
-        if(tor == ":" || tor.empty()){
-                 return "("+space+tor+space+")"+'\n';
+        if(torso == ":" || torso.empty()){
+                 return "("+space+torso+space+")"+'\n';
         }
-        
-        return "("+tor+")"+'\n';
-
+        return "("+torso+")"+'\n';
 }
-string insertBase(char num){
+
+//level 4 of the snowman
+string makeBase(char num){
         int base_i = (num-'0')-1;
-        string ba = body[base_i];
         string space = " ";
-        if (ba == ":"){
-                return "("+space+ba+space+")";
+        if (body[base_i] == ":"){
+                return "("+space+body[base_i]+space+")";
         }
-        if(ba.empty()){
-                return "("+space+ba+space+space+")";
+        if(body[base_i].empty()){
+                return "("+space+body[base_i]+space+space+")";
         }
-        
-        return "("+ba+")";
-        
+        return "("+body[base_i]+")";
 }
 
 string snowman(int num){
-    string number = to_string(num);
-    string error= "Invalid code ";
-    if(number.length() < 8 || number.length() > 8){
-      throw  invalid_argument(error + "'" + to_string(num) + "'");
+    //convert the num to array
+    string numArr = to_string(num);
+    //error msg
+    string errorMsg= "Invalid code ";
+
+    //check that it positive number
+        if (num < 0) {
+            throw out_of_range("the number must be positive!");
+        }
+     //check the length of the input - should be eight numbers
+    if(numArr.length() < eight || numArr.length() > eight){
+      throw  invalid_argument(errorMsg + "'" + to_string(num) + "'");
     }
-    for (int i = 0 ; i< number.length() ; i++){
-        int a = number[i]-'0';
+
+    //check that all the eight numbers between 1-4
+    for (int i = 0 ; i< numArr.length() ; i++){
+        int a = numArr[i]-'0';
         if(a <1 || a > 4){
-          throw  invalid_argument(error + "'" + to_string(num) + "'");
+          throw  invalid_argument(errorMsg + "'" + to_string(num) + "'");
         }
     }
-    string head = makeHead(number[0]);
-    string eyesAndNose = makeFace(number[1],number[2],number[3]);
-    string tor = insertTorso(number[6]);
-    string base = insertBase(number[7]);
+
+    //first level of the snowman
+    string head = printHead(numArr[zero]);
+    //second level of the snowman
+    string EyeAndNose = printFace(numArr[one],numArr[two],numArr[three]);
+    //third level of the snowman
+    string torso = makeTorso(numArr[six]);
+    //fourth level of the snowman
+    string base = makeBase(numArr[seven]);
 
     //check left up arm:
-    if (number[4] == '2'){
-      eyesAndNose = LArms[(number[4]-'0')-1] + eyesAndNose;
+    if (numArr[four] == '2'){
+      EyeAndNose = LArms[(numArr[four]-'0')-1] + EyeAndNose;
     }
     //check right up arm:
-    if (number[5] == '2'){
-      eyesAndNose.erase(eyesAndNose.size() - 1);
-      eyesAndNose += RArms[(number[5]-'0')-1] + '\n';
+    if (numArr[five] == '2'){
+      EyeAndNose.erase(EyeAndNose.size() - 1);
+      EyeAndNose += RArms[(numArr[five]-'0')-1] + '\n';
     }
     //check torso down left arm:
-    if (number[4] == '3' || number[4] == '1' || number[4] == '4'){
-      tor = LArms[(number[4]-'0')-1] + tor;
+    if (numArr[four] == '1' || numArr[four] == '3' || numArr[four] == '4'){
+      torso = LArms[(numArr[four]-'0')-1] + torso;
     }
     //check torso down right arm:
-    if (number[5] == '3' || number[5] == '1' || number[5] == '4'){
-      tor.erase(tor.size() - 1);
-      tor += RArms[(number[5]-'0')-1]+'\n';
+    if (numArr[five] == '1' || numArr[five] == '3' || numArr[five] == '4'){
+      torso.erase(torso.size() - 1);
+      torso += RArms[(numArr[five]-'0')-1]+'\n';
     }
-    string snowm = head+eyesAndNose+tor+base;
-    return snowm;
+
+    //final snowman
+    string snowman = head+EyeAndNose+torso+base;
+    return snowman;
   }
 
 
 }
-
